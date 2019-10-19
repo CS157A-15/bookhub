c7a7b998ddf4be607197b2e2f88bea0ab9e18bff
Xinru <xinrulin99@gmail.com>
2019-10-19 16:24:26 -0700

    started creating the website

---
+++ website/backend/routes/html-routes.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const mysql = require('mysql');
+
+module.exports =  function (app, connection) {
+    app.get('/', function(red, res) {
+        connection.query('SELECT * FROM cs157s.emp', function(err, data) {
+            (err)? res.send(err): res.json({users: data});
+        });
+    });
+}
