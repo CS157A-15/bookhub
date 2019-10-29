e90a08dde41a3d5f15da288ffe83b9ec058f498b
Xinru <xinrulin99@gmail.com>
2019-10-29 11:58:26 -0700

    the backend can not write to the frontend!! yay!

--- website/backend/client/src/App.js
+++ website/backend/client/src/App.js
@@ -18,15 +18,21 @@ export default class App extends React.Component {
     .catch(error => console.log(error));
   }
 
-  // showPeople = p => <div key={p.id}>{p.name}</div>
+  showPeople = p => <div key={p.id}>{p.name}</div>
   
 
   render() {
-      
+    const peeps = this.state.people.emp;
+    console.log(Array.isArray(peeps))
+    if( Array.isArray(this.state.people.emp)) console.log(peeps[0].id)
+
+
+   const printThisPlease = Array.isArray(this.state.people.emp)? (peeps.map(p => <div key={p.id}>{p.name}</div>) ):( <div></div>)
+
     return ( 
       <div className= "App">
         <h1>testing</h1>
-
+        {printThisPlease}
       </div>
     );
   }
