9edfc9e2d60a863e09e1fc02914d2dea1d646290
Xinru <xinrulin99@gmail.com>
2019-12-03 22:39:39 -0800

    added the search bar and updated node modules

--- website/bookhub_frontend/src/pages/navbar/navbar.js
+++ website/bookhub_frontend/src/pages/navbar/navbar.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
-        MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBBtn} from "mdbreact";
+import {
+  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
+  MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBBtn
+} from "mdbreact";
 import { BrowserRouter as Router, Redirect } from 'react-router-dom';
 import UserAuth from '../../user_auth';
 
 class Navbar extends Component {
-state = {
-  isOpen: false,
-  authorize: UserAuth.getAuth()
-};
+  state = {
+    isOpen: false,
+    authorize: UserAuth.getAuth()
+  };
 
-toggleCollapse = () => {
-  this.setState({ isOpen: !this.state.isOpen });
-}
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
 
   _handleSearch = e => {
     console.log('handleSearch from navbar');
@@ -31,19 +33,19 @@ toggleCollapse = () => {
     return this.state.books;
   }
 
-auth= _=>{
-  UserAuth.setAuth(false);
-  this.setState({authorize:UserAuth.getAuth()});
-}
-
-
-
-render() {
-  if (this.state.authorize == false) {
-    return <Redirect push to="/" />;
+  auth = _ => {
+    UserAuth.setAuth(false);
+    this.setState({ authorize: UserAuth.getAuth() });
   }
 
-  return (
+
+
+  render() {
+    if (this.state.authorize == false) {
+      return <Redirect push to="/" />;
+    }
+
+    return (
       <MDBNavbar color="indigo" dark expand="md">
         <MDBNavbarBrand>
           <strong className="white-text">Bookhub</strong>
@@ -63,12 +65,20 @@ render() {
           </MDBNavbarNav>
           <MDBNavbarNav right >
             <MDBNavbarNav right>
-            <MDBBtn color="primary" onClick={this.auth} type="submit">Logout</MDBBtn>
+              <MDBFormInline waves>
+                <div className="md-form my-0">
+                  <input className="form-control mr-sm-2" type="text"
+                    placeholder="Search" aria-label="Search"
+                    onChange={e => { this.props.handleSearch(e); }}
+                    onKeyPress={e => { this.props.enterPressed(e); }} />
+                </div>
+              </MDBFormInline>
+              <MDBBtn color="primary" onClick={this.auth} type="submit">Logout</MDBBtn>
             </MDBNavbarNav>
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBNavbar>
-    
+
     );
   }
 }
